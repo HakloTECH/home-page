6954c2b1955e7fc2c3170591f0a8182807f4fee6
Momijiichigo <ichigomomiji436@gmail.com>
2023-01-31 11:22:52 -0600

    better header background

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import withBlue from 'vite-with-bluejsx'
 import viteTsUrlLoader from './vite-ts-url-loader'
 export default withBlue({
   bluejsx: {
-    hmr: true,
+    hmr: false,
   },
   plugins: [
     viteTsUrlLoader()
